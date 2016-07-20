903e4c8c0e8aeee811dfb192a2fe76139adeb037
Alexei Yuzhakov <ayuzhakov@plesk.com>
2016-07-20 17:12:24 +0600

    Update copyright year.

--- plib/library/FileManager/Action.php
+++ plib/library/FileManager/Action.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 1999-2015. Parallels IP Holdings GmbH. All Rights Reserved.
+// Copyright 1999-2016. Parallels IP Holdings GmbH. All Rights Reserved.
 class Modules_RichEditor_FileManager_Action extends pm_FileManager_Action
 {
     public function getTitle()
