d6a30571469790600468358346b4865e25499c96
Alexei Yuzhakov <sibprogrammer@gmail.com>
2017-03-15 20:55:30 +0700

    Update copyright year.

--- Capfile
+++ Capfile
@@ -1,4 +1,4 @@
-### Copyright 1999-2016. Parallels IP Holdings GmbH. All Rights Reserved.
+### Copyright 1999-2017. Parallels IP Holdings GmbH. All Rights Reserved.
 
 server ENV['HOST'], :web
 set :user, 'root'
--- htdocs/index.php
+++ htdocs/index.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 1999-2016. Parallels IP Holdings GmbH. All Rights Reserved.
+// Copyright 1999-2017. Parallels IP Holdings GmbH.
 pm_Context::init('rich-editor');
 
 $application = new pm_Application();
